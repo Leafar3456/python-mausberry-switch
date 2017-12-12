136fbb9b0cac39b857a76f9301a2a55d70a772c6
Rafael Schridi <r.schridi@gmail.com>
2017-12-12 02:40:03 +0000

    make non-resource intesive and use other script

--- python/nespi.py
+++ python/nespi.py
@@ -1,44 +1,28 @@
+#!/usr/bin/env python
+
 # Import the RPi.GPIO and OS
 import RPi.GPIO as GPIO
 import os
-import sys
-import time
+import subprocess
 
 # GPIO port setup
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 # Power switch: will send a shutdown message to the OS
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-# Reset switch: will look for a running emulator process, and send it an interrupt signal
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # 16 = 23
 
-# Mausberry circuit: when this pin goes from 1 to 0, the circuit cuts the power to the pi
-GPIO.setup(24, GPIO.OUT)
-GPIO.output(24, 1)
+# Mausberry circuit: when this pin goes from 1 to 0, the circuit cuts the power to the pi when shut down
+GPIO.setup(18, GPIO.OUT) # 18 = 24
+GPIO.output(18, 1)
 
-# Looks for an emulator process and send an interrupt signal if found
-def exitEmulator(channel):
-    print('exitEmulator')
-    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+# pause the script and wait for rising pin
+GPIO.wait_for_edge(16, GPIO.RISING)
 
-    for pid in pids:
-        try:
-            commandpath = open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
-            if commandpath[0:24] == '/opt/retropie/emulators/':
-                os.system('kill -QUIT %s' % pid)
-                print('kill -QUIT %s' % pid)
-        except IOError:
-            continue
+# enable if using killes.sh from 
+#https://retropie.org.uk/forum/topic/12895/ensuring-es-gracefully-finish-and-save-metadata-in-every-system-shutdown
+#cmd = ['/etc/killes.sh']
+#subprocess.Popen(cmd,shell=True).wait()
 
 # Sends a poweroff command to the OS
-def powerOff(channel):
-    print('poweroff')
-    os.system('poweroff')
-
-# Here we just wire the  GPIO inputs to their respective callback functions
-GPIO.add_event_detect(23, GPIO.RISING, callback=powerOff, bouncetime=500)
-GPIO.add_event_detect(25, GPIO.RISING, callback=exitEmulator, bouncetime=500)
-
-# And... nothing more to do. let's wait.
-while True:
-    time.sleep(10)
+print('triggering shutdown by hardware')
+os.system('poweroff')
